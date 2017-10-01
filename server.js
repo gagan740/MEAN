f97cb9944c2068e83e11a2839ce65e1afcef09db
Gagan740 <gagan@codehelperz.com>
2017-10-01 22:19:25 +0530

    updates

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const express   =   require('express');
-const app       =   express();
-const mongoose  =   require('mongoose');
-const config    =   require('./config/database');
-const path      =   require('path');
+const express           =   require('express');
+const app               =   express();
+const router            =   express.Router();
+const mongoose          =   require('mongoose');
+const config            =   require('./config/database');
+const path              =   require('path');
+const bodyParser        =   require('body-parser');
+const authentication    =   require('./routes/authentication')(router);
 
-mongoose.Promise    =   global.Promise;
-mongoose.connect(config.uri, (err) => {
+mongoose.connect(config.uri, { useMongoClient: true, promiseLibrary: global.Promise }, (err) => {
     if (err) {
         console.log('Could not connect to database: ', err);
     } else {
-        console.log(config.secret);
         console.log(`Connected to database ${config.db}`);
     }
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/client/dist/'));
+app.use('/authentication', authentication);
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname + '/client/dist/index.html'));
